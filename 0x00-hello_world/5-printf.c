b32af9ef575d70b35720cc3ecce1aa7302a3245a
Adepoju Lateef <ademustexcel@gmail.com>
2022-03-12 21:50:45 -0800

    added void, added return comments

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,9 @@
  * main - prints the phrase with proper
  * grammar, but the outcome is a piece
  * of art.
+ * Return: 0 if exited properly, non-zero otherwise
  */
-int main()
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
