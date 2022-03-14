4480a8caa3bd9bdee552f90241f220aca77ebcad
Adepoju Lateef <ademustexcel@gmail.com>
2022-03-14 12:55:30 -0700

    3 print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * and then in uppercase, followed by a 
+ * new line.
+ * Return: return 0 if success
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z', ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
