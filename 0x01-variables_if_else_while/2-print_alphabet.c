9f038540fe2545f34c894d10b5cd6cefbfde3207
Adepoju Lateef <ademustexcel@gmail.com>
2022-03-14 12:31:46 -0700

    2 print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints th letter in lowercase,
+ * followed by a new line
+ * Return: return 0 if success
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'a'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+
+}
