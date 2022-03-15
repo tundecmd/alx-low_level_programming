a98b3cf5c3e9d4ac47ef0a29ad67fe2028645ab9
Adepoju Lateef <ademustexcel@gmail.com>
2022-03-15 07:24:45 -0700

    0 putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar followed by
+ * a new line
+ * Return: return 0 if success
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	return (0);
+}
