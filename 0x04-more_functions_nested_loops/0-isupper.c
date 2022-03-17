4b6509e94c9f2849195c376e415607071fcdf3d9
Adepoju Lateef <ademustexcel@gmail.com>
2022-03-17 10:51:36 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.c"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: return 1 if c is uppercase, 0 if 
+ * otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
