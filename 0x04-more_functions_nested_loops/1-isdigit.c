612626a24e2c095e158aa5776abe8a56881347b6
Adepoju Lateef <ademustexcel@gmail.com>
2022-03-19 08:25:06 +0100

    1 digit added

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - fuction that check if a number is digit from 0-9
+ * @c: integer parameter
+ * Return: integer number
+ **/
+int _isdigit(int c)
+{
+  return (c >= '0' && c <= '9');
+}
