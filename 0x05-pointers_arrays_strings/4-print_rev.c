d422e3fad66e61f70d6c6aaac6ea800fd00ce67b
Adepoju Lateef <ademustexcel@gmail.com>
2022-03-22 01:12:27 -0700

    4 print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	/* Get the length of the string */
+	while (1 == 1)
+	{
+		if (*(s + len) == '\0')
+		break;
+		len++;
+	}
+	for (--len; len >= 0; len--)
+	{
+		_putchar(s[len]);
+		_putchar('\n');
+	}
+}
