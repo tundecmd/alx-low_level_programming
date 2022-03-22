71ebf20d3dc7c6e0ed2b1cb22af0aaecad6dd641
Adepoju Lateef <ademustexcel@gmail.com>
2022-03-22 00:37:55 -0700

    completed main

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen - Function that returns the length of a string
+ * @s: char parameter
+ * Return: integer string of a string
+ **/
+int _strlen(char *5)
+{
+	return (strlen(5));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,18 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
+#include <stdio.h>
+#include <string.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+int _putchar(char c);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
 #endif
