12b366e2dc3e15d373369515d74201bb821f1949
Adepoju Lateef <ademustexcel@gmail.com>
2022-03-22 00:51:49 -0700

    3 puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts - A function that prints a string followed by a new line to stdout.
+ * @str: string parameter
+ * Return: void
+ **/
+void _puts(char *str)
+{
+	puts(str);
+}
