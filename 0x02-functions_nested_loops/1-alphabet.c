ac00c0e44b33843d4cc20ae5da2d4585afae2de6
Adepoju Lateef <ademustexcel@gmail.com>
2022-03-23 05:39:45 +0100

    added 1 alphabet c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - a function that print all alphabets in lower case
+ * Return: void
+ **/
+void print_alphabet(void)
+{
+int alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+  _putchar(alpha);
+  _putchar('\n');
+}
