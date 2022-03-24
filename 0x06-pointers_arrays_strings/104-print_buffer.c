6f1bdfb6eda7464931860f3fd351d92add18f9a7
Adepoju Lateef <ademustexcel@gmail.com>
2022-03-24 09:48:24 -0700

    indentation

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -15,7 +15,8 @@ void print_buffer(char *b, int size)
 	else
 	{
 		for (i = 9, pi = 0; i < size; i += 10)
-		{	printf("%.8x: ", pi);
+		{	
+			printf("%.8x: ", pi);
 				for (c = pi; c <= i ; c++)
 				{	printf("%.2x", b[c]);
 					if (c % 2 == 1)
@@ -28,11 +29,12 @@ void print_buffer(char *b, int size)
 					else
 						printf("%c", b[c]);	
 				}
-					printf("\n");
-					pi = i + 1;	
+				printf("\n");
+				pi = i + 1;	
 		}
 		if (pi < size - 1)
-		{	i -= 9;
+		{	
+			i -= 9;
 			printf("%.8x: ", i);
 			for (c = i; c < i + 10; c++)
 			{
