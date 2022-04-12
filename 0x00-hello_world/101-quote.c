71d29e863b45e0c820bbefa2940379cef2a42a97
Adepoju Lateef <ademustexcel@gmail.com>
2022-04-12 07:07:44 -0700

    added description of the main

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/*
+ * main - prints some string to the standard error.
+ *
+ * Return: return 1.
+ *
+ */
+
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
