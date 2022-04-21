cb9c2dfa5e7601e80a6f72ebee87b2f24b23aae6
Adepoju Lateef <ademustexcel@gmail.com>
2022-04-21 04:07:17 -0700

    103 fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,21 +7,21 @@
  */
 int main(void)
 {
-  long int pf, af, f, s;
+long int pf, af, f, s;
 
-  s = 2;
-  pf = 1;
-  af = 2;
-  while ((af + pf) <= 4000000)
-  {
-    f = pf + af;
-    pf = af;
-    af = f;
-    if ((f % 2) == 0)
-    {
-      s = s + f;
-    }
-  }
-  printf ("%ld\n", s);
-  return (0);
+s = 2;
+pf = 1;
+af = 2;
+while ((af + pf) <= 4000000)
+{
+f = pf + af;
+pf = af;
+af = f;
+if ((f % 2) == 0)
+{
+s = s + f;
+}
+}
+printf ("%ld\n", s);
+return (0);
 }
